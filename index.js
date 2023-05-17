f3924499c640944fa344914a83a14e47cc195309
Jhonny Mauricio <jmauricio@solera.pe>
2023-05-16 22:09:06 -0500

    add configuration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });
+    res.status(201).json({ message: 'Hello World' });
 })
 
 app.listen(3000, () => { console.log('Server is running on port 3000') })
